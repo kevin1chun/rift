efee5203b91b95338c19935ef43d2e89acbbe8c3
jerluc <jeremyalucas@gmail.com>
2015-04-20 23:37:37 -0700

    Fixing string escape literals

--- src/rift/lang/value.go
+++ src/rift/lang/value.go
@@ -1,15 +1,54 @@
 package lang
 
 import (
+	"bytes"
 	"strconv"
 	"rift/support/sanity"
 )
 
-// TODO: Actually, is there a difference between a string node
-// vs. the string value representing a named ref?
 func (stringNode *Node) Str() string {
 	sanity.Ensure(stringNode.Type == STRING, "Invalid cast from type [%s] to [%s]", stringNode.Type, STRING)
-	return stringNode.Values[0].(string)
+	origStr := stringNode.Values[0].(string)
+
+	var buffer bytes.Buffer
+	var escaped bool
+	for _, c := range origStr {
+		if escaped {
+			switch c {
+			case '\\':
+				buffer.WriteRune('\\')
+			case '\'':
+				buffer.WriteRune('\'')
+			case '?':
+				buffer.WriteRune('?')
+			case '"':
+				buffer.WriteRune('"')
+			case 'a':
+				buffer.WriteRune('\a')
+			case 'b':
+				buffer.WriteRune('\b')
+			case 'f':
+				buffer.WriteRune('\f')
+			case 'n':
+				buffer.WriteRune('\n')
+			case 'r':
+				buffer.WriteRune('\r')
+			case 't':
+				buffer.WriteRune('\t')
+			case 'v':
+				buffer.WriteRune('\v')
+			}
+			escaped = false
+			continue
+		}
+		if c == '\\' {
+			escaped = true
+		} else {
+			buffer.WriteRune(c)
+		}
+	}
+
+	return buffer.String()
 }
 
 func (numericNode *Node) Int() int {
